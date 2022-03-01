fafacc7e3ba8e5fcd24d90f160d1c8548cadaab0
Suraj-HM <alpha7.suraj@gmail.com>
2022-03-01 22:58:54 +0530

    daily practice

---
+++ 056_needle_in_haystack.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(needle.length() == 0) return 0;
+        if(haystack.length() == 0) return -1;
+        for(int i = 0; i < haystack.length() - needle.length() + 1; i++) {
+            boolean flag = false;
+            for(int j = 0; j < needle.length(); j++) {
+                if(haystack.charAt(i+j) == needle.charAt(j)) flag = true;
+                else {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag) return i;
+        }
+        return -1;
+    }
+}
